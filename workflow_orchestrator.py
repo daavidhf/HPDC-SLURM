aa890dfcfc7c36f3f11f5dedc02c6f8d12a7e8f1
David <hidalgo.fabregas.david@gmail.com>
2026-04-23 23:21:46 +0200

    complete  version of consolidate_results.py and workflow_orchestrator.py

--- workflow_orchestrator.py
+++ workflow_orchestrator.py
@@ -6,34 +6,41 @@ from datetime import datetime
 import subprocess
 
 def main():
+    # ==========================================================
+    # PHASE 1: ANALYSIS OF DATA FOLDERS AND PREPARATION OF TASKS
+    # ==========================================================
+
     # 1. Detect the project path (pass it as an argument or use the current one)
     # If an argument is provided, we use it as the base directory
+    code_dir = os.path.dirname(os.path.abspath(__file__))
     if len(sys.argv) > 1:
         base_dir = os.path.abspath(sys.argv[1])
-    # If no argument is provided, we assume the script must be ran using the directory where it is located
+    # If no argument is provided, script uses the directory where it is located
     else:
-        base_dir = os.path.dirname(os.path.abspath(__file__))
+        base_dir = code_dir
 
-    # Define the relative paths based on the statement
+    # Define the relative paths
     data_dir = os.path.join(base_dir, 'data')
     logs_dir = os.path.join(base_dir, 'logs')
     results_dir = os.path.join(base_dir, 'results')
     scripts_dir = os.path.join(base_dir, 'scripts')
 
+
     # 2. Configure the logging system (ORCHEST_LOG.txt)
     log_file = os.path.join(logs_dir, 'ORCHEST_LOG.txt')
     with open(log_file, 'w') as f: # 'w' for write mode, which will overwrite the file if it already exists
-        title = " STARTING ORCHESTRATOR "
+        title = " STARTING PHASE 1: EXPLORATION AND TASKS PREPARATION "
         f.write(f"{title.center(100, '=')}\n")
-    
+
     def register_log(message):
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         full_message = f"[{current_time}] {message}"
-        print(full_message)  # Print to console as well
+        print(full_message)
         with open(log_file, 'a') as f: # 'a' for append mode without overwriting
             f.write(full_message + '\n')
     
-    register_log(f"Base directory: {base_dir}")
+    register_log(f"Project directory: {base_dir}")
+
 
     # 3. Explore the "sample" folders and extract intervals
     intervals = []
@@ -53,13 +60,13 @@ def main():
         # Search for ranges_x.txt inside the current folder
         range_files = sorted(glob.glob(os.path.join(folder, 'ranges_*.txt')))
         
-        for rf in range_files:
-            file_name = os.path.basename(rf)
+        for file in range_files:
+            file_name = os.path.basename(file)
             register_log(f"  -> Reading range file: {file_name}")
             
-            with open(rf, 'r') as file:
-                for line in file:
-                    # Strip to remove leading/trailing whitespace and split by spaces
+            with open(file, 'r') as f:
+                for line in f:
+                    # Strip to remove leading whitespace and split by spaces
                     parts = line.strip().split()
                     # If the line has two numbers, we save it
                     if len(parts) == 2:
@@ -67,7 +74,8 @@ def main():
 
     register_log(f"Exploration completed. {len(intervals)} intervals found in total.")
     
-    # 4. Save all intervals in a master temporary file for SLURM
+
+    # 4. Save all intervals in a temporary file for SLURM
     tasks_file = os.path.join(results_dir, 'all_intervals.txt')
     with open(tasks_file, 'w') as f:
         f.write('\n'.join(intervals) + '\n')
@@ -75,20 +83,22 @@ def main():
     register_log(f"Tasks file generated at: {tasks_file}")
 
     with open(log_file, 'a') as f: # 'a' for append mode without overwriting
-        title = " END OF EXPLORATION PHASE "
+        title = " END OF PHASE 1: EXPLORATION AND TASKS PREPARATION "
         f.write(f"{title.center(100, '=')}\n")
+
 
 
     # ===========================================
     # PHASE 2: GENERATION AND SUBMISSION TO SLURM
     # ===========================================
-    fase_slurm = " STARTING SLURM PHASE "
-    register_log(fase_slurm.center(100, '='))
+    title = " STARTING PHASE 2: SLURM FILES GENERATION AND JOBS SUBMISSION "
+    register_log(title.center(100, '='))
 
     num_tasks = len(intervals)
     if num_tasks == 0:
         register_log("No tasks to execute. Aborting SLURM phase.")
         return
+
 
     # 1. Create the Bash script for the Job Array calculation
     compute_sh = os.path.join(scripts_dir, 'compute_primes.sh')
@@ -96,22 +106,38 @@ def main():
         f.write(
             f"""
                 #!/bin/bash
+
+                # Set the job name for easier identification in the queue
                 #SBATCH --job-name=calc_primes
+
+                # Set the array to run from 1 to the number of tasks (intervals) we have at the same time.
+                # SLURM will automatically set the environment variable SLURM_ARRAY_TASK_ID to the current index (1-based).
                 #SBATCH --array=1-{num_tasks}
+
+                # Output and error files for each task in the array, using the task ID to differentiate them.
+                # %A is the job ID and %a is the array index. Each task will have its own log files.
                 #SBATCH --output={logs_dir}/compute_%A_%a.out
                 #SBATCH --error={logs_dir}/compute_%A_%a.err
 
-                # Extract start and end of the interval corresponding to this task
+                # Extract start and end of the interval corresponding to this task:
+                # sed "stream editor" gets the line corresponding to the current task ID from the tasks file.
+                # -n silent mode, only print what we explicitly ask for.
+                # p explicitly print the line that matches the task ID.
                 LINE=$(sed -n "${{SLURM_ARRAY_TASK_ID}}p" {tasks_file})
+
+                # awk search for witespaces to split the line and print field 1 and 2 (start and end).
                 START=$(echo $LINE | awk '{{print $1}}')
                 END=$(echo $LINE | awk '{{print $2}}')
 
-                # Run the original script, filter the total, and save the partial results
+                # Run the count_tot_primes script, filter the total, and save the partial results
+                # grep: selects the line that contains the total count, so we only save the primes found in the interval.
+                # -v option: inverts the match, so it prints all lines that do NOT contain "TOTAL:".
                 python {scripts_dir}/count_tot_primes.py $START $END | grep -v "TOTAL:" > {results_dir}/primes_${{SLURM_ARRAY_TASK_ID}}.txt
             """
         )
 
     register_log(f"Generated computing script: {compute_sh}")
+
 
     # 2. Create the Bash script for the consolidation
     merge_sh = os.path.join(scripts_dir, 'merge_primes.sh')
@@ -119,41 +145,50 @@ def main():
         f.write(
             f""" 
                 #!/bin/bash
+
+                # Set the job name for easier identification in the queue
                 #SBATCH --job-name=merge_primes
+
+                # Output and error files
+                # $j is the job ID assigned by SLURM to this job, we use it to differentiate the logs.
                 #SBATCH --output={logs_dir}/merge_%j.out
                 #SBATCH --error={logs_dir}/merge_%j.err
 
                 # Run our auxiliary Python script to merge the results
-                python {scripts_dir}/consolidate_results.py {results_dir}
+                python {code_dir}/scripts/consolidate_results.py {results_dir}
             """
         )
 
     register_log(f"Generated consolidation script: {merge_sh}")
 
-    # 3. Submit the jobs to SLURM (Protected in case we are on local)
-    try:
-        # Submit the first job (Array)
-        res1 = subprocess.run(['sbatch', compute_sh], capture_output=True, text=True, check=True)
-        # sbatch returns something like "Submitted batch job 12345", we extract the ID:
-        job1_id = res1.stdout.strip().split()[-1]
-        register_log(f"Job Array submitted to SLURM successfully. Job ID: {job1_id}")
 
-        # Submit the second job (Dependent on the first)
-        # We use --dependency=afterok:ID so it waits for the Array to finish successfully before starting
-        dependency = f"--dependency=afterok:{job1_id}"
-        res2 = subprocess.run(['sbatch', dependency, merge_sh], capture_output=True, text=True, check=True)
-        job2_id = res2.stdout.strip().split()[-1]
-        register_log(f"Consolidation job submitted to SLURM. Job ID: {job2_id} (Depends on {job1_id})")
+    # 3. Submit the jobs to SLURM
 
-    except FileNotFoundError:
-        register_log("WARNING: The command 'sbatch' does not exist in this system.")
-        register_log("This is normal if you are testing on your local computer (WSL).")
-        register_log("The .sh files have been generated correctly for when you upload the code to the cluster.")
-    except subprocess.CalledProcessError as e:
-        register_log(f"ERROR: Failed to communicate with SLURM. Details: {e.stderr}")
+    # Submit the first job (Array)
+    res1 = subprocess.run(['sbatch', compute_sh], # call sbatch with the compute_sh script to submit the job to SLURM
+                            capture_output=True, # Not printing the output directly, save it in res1.stdout and res1.stderr
+                            text=True, # Interpret the output as text (string) instead of bytes
+                            check=True # If command fails (non-zero exit code), raise a CalledProcessError exception
+                            )
+    
+    # sbatch returns something like "Submitted batch job 12345", we extract the ID:
+    job1_id = res1.stdout.strip().split()[-1]
+    register_log(f"Job Array submitted to SLURM successfully. Job ID: {job1_id}")
 
-    fase_fin = " END OF ORCHESTRATION "
-    register_log(fase_fin.center(80, '='))
+    # Submit the second job (Dependent on the first one)
+    dependency = f"--dependency=afterok:{job1_id}" # wait for the Array (first job) to finish successfully before starting
+    res2 = subprocess.run(['sbatch', dependency, merge_sh],
+                            capture_output=True, # Not printing the output directly, save it in res2.stdout and res2.stderr
+                            text=True, # Interpret the output as text (string) instead of bytes
+                            check=True # If command fails (non-zero exit code), raise a CalledProcessError exception
+                            )
+    
+    job2_id = res2.stdout.strip().split()[-1]
+    register_log(f"Consolidation job submitted to SLURM. Job ID: {job2_id} (Depends on {job1_id})")
+
+    title = " END OF PHASE 2: SLURM FILES GENERATION AND JOBS SUBMISSION "
+    register_log(title.center(100, '='))
+
 
 if __name__ == '__main__':
     main()
