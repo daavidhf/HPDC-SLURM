e8f7e4c747b43c77dc47e2315a594850e6e519b5
David <hidalgo.fabregas.david@gmail.com>
2026-04-28 17:17:44 +0200

    final execution

--- scripts/consolidate_results.py
+++ scripts/consolidate_results.py
@@ -62,7 +62,7 @@ def main():
     register_log(f"Success: {len(sorted_primes)} unique primes saved in {final_file}")
 
     with open(log_file, 'a') as f: # 'a' for append mode, which will add to the file that already exists
-        title = " END OF PHASE 3: RESULTS "
+        title = " END OF PHASE 4: RESULTS "
         f.write(f"{title.center(100, '=')}\n")
 
 if __name__ == '__main__':
